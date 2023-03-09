36b490f630ec8b6e1c1393f188dceda0efd2ec41
Dylan <1395855727@qq.com>
2023-03-09 21:48:00 +0800

    add no9 to no12

---
+++ homework/demo.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Dylan on 2023/3/9.
+//
+
+
+#include <iostream>
+using namespace std;
+
+void fun(char* a, const char* b){
+    while (*b)*a++=*++;
+    *a=0;
+}
+
+int main(){
+
+
+    return 0;
+}
